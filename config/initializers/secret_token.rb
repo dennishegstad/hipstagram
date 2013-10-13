8d3522e7912d29b675a142bc69b7bb19201af272
dennis hegstad <dennis@yespix.com>
2013-10-13 15:24:35 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hipstagram::Application.config.secret_key_base = 'b7cf889746504b297bd8259f47b60f947d0172dbadc8efe54a878071836d7def5da87c8acc6cd81891fc3fbc837db562f51fc6b69c95d37d47658378a5f8892d'
